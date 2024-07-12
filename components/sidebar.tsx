40263c8be2f52c5fd6cc5c8934991c3307abb686
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-07-12 12:09:02 +0545

    sidebar-href

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,18 +3,29 @@
 import React from 'react'
 import logo from '@/public/logo.png'
 import Image from 'next/image'
+import Link from 'next/link'
+import { sidebarItems } from '@/data'
 
 const Sidebar = () => {
   return (
     <div className=''>
         <nav>
-            <div className='flex flex-col '>
+            <div className='flex flex-col  divide-x'>
                <Image src={logo} alt='' width={60} height={60} />
-               <div>
-                <h2>Ujjwal</h2>
+               <div className=''> 
+                <h2 className=''>Ujjwal</h2>
                 <p>Your Money</p>
                </div>
-               <div>
+               <hr></hr>
+               <div className="ml-4 flex flex-col gap-y-4 text-sm text-muted-foreground">
+                {sidebarItems.map((items,index)=>(
+                  <Link 
+                  href={items.href}
+                  key={index}>
+                    {items.lable}
+                  </Link>
+                ))}
+
                 
                </div>
 
---
+++ data/index.ts
@@ -0,0 +1,22 @@
+export const sidebarItems = [
+    {
+        lable : "Dashboard",
+        href : "/dashboard",
+    },
+    {
+        lable : "Expence-Tracker",
+        href : "/expence-tracker",
+    },
+    {
+        lable : "Income",
+        href : "/income",
+    },
+    {
+        lable : "Saving",
+        href : "/saving",
+    },
+    {
+        lable : "Miscelleanous",
+        href : "/miscellenous",
+    },
+]
